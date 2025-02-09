cea1b8272c976af789251f0824ddbb4856276d97
capvamshi <98767180+capvamshi@users.noreply.github.com>
2025-02-09 12:18:36 +0000

    python updated for dataset 3

--- data_aggregator.py
+++ data_aggregator.py
@@ -1,12 +1,11 @@
 import pandas as pd
-
-dataset1 = pd.read_csv("data/data_main.csv")     # Dataset1 is the main data
+dataset1 = pd.read_csv("/workspaces/npci-mlops-assignment4-capvamshi/data/data_main.csv")     # Dataset1 is the main data
 
 # Dataset2 is the data you want to add
-dataset2 = pd.read_csv("month2_data.csv")        # Change the path to add month3_data.csv 
+dataset3 = pd.read_csv("/workspaces/npci-mlops-assignment4-capvamshi/month3_data.csv")        # Change the path to add month3_data.csv 
 
 # Append rows of dataset2 to dataset1
-dataset1 = pd.concat([dataset1, dataset2],axis=0, ignore_index=True)
+dataset1 = pd.concat([dataset1, dataset3],axis=0, ignore_index=True)
 
 # Overwrite data_main.csv file
-dataset1.to_csv("data/data_main.csv", index=False) 
+dataset1.to_csv("/workspaces/npci-mlops-assignment4-capvamshi/data/data_main.csv", index=False) 
